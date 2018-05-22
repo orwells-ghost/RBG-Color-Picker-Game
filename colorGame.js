0b67b23141fa6baee928253c610e253e7a04967c
Ryan Kennedy <33361057+orwells-ghost@users.noreply.github.com>
2018-05-21 18:45:46 -0700

    Added easy and hard buttons w/ JS event listeners

--- colorGame.js
+++ colorGame.js
@@ -5,7 +5,37 @@ var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
-var resetButton = document.querySelector("#reset")
+var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+easyBtn.addEventListener("click", function(){
+  easyBtn.classList.add("selected");
+  hardBtn.classList.remove("selected");
+  colors = generateRandomColors(3);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for(var i = 0; i <squares.length; i++){
+    if(colors[i]){
+      squares[i].style.backgroundColor = colors[i];
+    }
+    else {
+      squares[i].style.display = "none";
+    }
+  }
+});
+
+hardBtn.addEventListener("click", function(){
+  hardBtn.classList.add("selected");
+  easyBtn.classList.remove("selected");
+  colors = generateRandomColors(6);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for(var i = 0; i <squares.length; i++){
+      squares[i].style.backgroundColor = colors[i];
+      squares[i].style.display = "block";
+  }
+});
 
 resetButton.addEventListener("click", function(){
   //generate all new colors
@@ -18,6 +48,7 @@ resetButton.addEventListener("click", function(){
   for(var i = 0; i < squares.length; i++){
     squares[i].style.backgroundColor = colors[i];
   }
+  h1.style.backgroundColor = "#232323"
 })
 
 colorDisplay.textContent = pickedColor;
@@ -33,6 +64,7 @@ for(var i = 0; i < squares.length; i++){
     //compared color to pickedColor
     if(clickedColor === pickedColor){
       messageDisplay.textContent = "Correct!";
+      resetButton.textContent = "Play Again?"
       changeColors(clickedColor);
       h1.style.backgroundColor = clickedColor;
     } else {
