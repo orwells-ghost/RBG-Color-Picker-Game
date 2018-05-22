5d3d3be0491ddd46d644398762b74632aeaf15be
Ryan Kennedy <33361057+orwells-ghost@users.noreply.github.com>
2018-05-21 18:18:57 -0700

    First commit.

---
+++ colorGame.js
@@ -0,0 +1,64 @@
+var colors = generateRandomColors(6);
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message")
+
+colorDisplay.textContent = pickedColor;
+
+for(var i = 0; i < squares.length; i++){
+  //add initial colors to squares
+  squares[i].style.backgroundColor = colors[i];
+
+  //add click listeners to squares
+  squares[i].addEventListener("click", function(){
+    //grab color of clicked square
+    var clickedColor = this.style.backgroundColor;
+    //compared color to pickedColor
+    if(clickedColor === pickedColor){
+      messageDisplay.textContent = "Correct!";
+      changeColors(clickedColor);
+    } else {
+      //fade out if wrong color
+      this.style.backgroundColor = "#232323";
+      messageDisplay.textContent = "Try Again";
+    }
+  });
+}
+
+function changeColors(color){
+  //loop through all squares
+  for(var i = 0; i < colors.length; i++){
+    squares[i].style.backgroundColor = color;
+  }
+  //change all squares to given color
+}
+
+function pickColor(){
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
+}
+
+function generateRandomColors(num){
+  //make an array
+  var array = [];
+  //repeat num times
+  for(var i = 0; i < num; i++){
+    //get random color and push into array
+    array.push(randomColor())
+  }
+  //return array
+  return array;
+}
+
+function randomColor(){
+  //pick a red from 0 to 255
+  var r = Math.floor(Math.random() * 256)
+  //pick green frmo 0 to 255
+  var g = Math.floor(Math.random() * 256)
+  //pick blue from 0 to 255
+  var b = Math.floor(Math.random() * 256)
+  //"rgb(r, g, b")
+  return "rgb(" + r + ", " + g + ", " + b + ")"
+}
