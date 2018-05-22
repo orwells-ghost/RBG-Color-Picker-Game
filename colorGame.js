97347e5a651a9555ab45f869864e58bb6104f6bb
Ryan Kennedy <33361057+orwells-ghost@users.noreply.github.com>
2018-05-21 19:12:32 -0700

    Added styles to the page

--- colorGame.js
+++ colorGame.js
@@ -1,5 +1,5 @@
-var colors = generateRandomColors(6);
-
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
@@ -9,10 +9,12 @@ var resetButton = document.querySelector("#reset");
 var easyBtn = document.querySelector("#easyBtn");
 var hardBtn = document.querySelector("#hardBtn");
 
+
 easyBtn.addEventListener("click", function(){
   easyBtn.classList.add("selected");
   hardBtn.classList.remove("selected");
-  colors = generateRandomColors(3);
+  numSquares = 3;
+  colors = generateRandomColors(numSquares);
   pickedColor = pickColor();
   colorDisplay.textContent = pickedColor;
   for(var i = 0; i <squares.length; i++){
@@ -28,7 +30,8 @@ easyBtn.addEventListener("click", function(){
 hardBtn.addEventListener("click", function(){
   hardBtn.classList.add("selected");
   easyBtn.classList.remove("selected");
-  colors = generateRandomColors(6);
+  numSquares = 6;
+  colors = generateRandomColors(numSquares);
   pickedColor = pickColor();
   colorDisplay.textContent = pickedColor;
   for(var i = 0; i <squares.length; i++){
@@ -39,7 +42,7 @@ hardBtn.addEventListener("click", function(){
 
 resetButton.addEventListener("click", function(){
   //generate all new colors
-  colors = generateRandomColors(6);
+  colors = generateRandomColors(numSquares);
   //pick a new random color from array
   pickedColor = pickColor();
   //change colorDisplay to match picked color
@@ -48,7 +51,7 @@ resetButton.addEventListener("click", function(){
   for(var i = 0; i < squares.length; i++){
     squares[i].style.backgroundColor = colors[i];
   }
-  h1.style.backgroundColor = "#232323"
+  h1.style.backgroundColor = "steelblue"
 })
 
 colorDisplay.textContent = pickedColor;
